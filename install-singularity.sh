35390be0b734f07019eeb59480c16f7ca7a21a66
Shakir <shakirali.sacube@gmail.com>
2024-10-23 18:23:14 +0000

    Improved singularity install script to leave no install artifacts behind

--- install-singularity.sh
+++ install-singularity.sh
@@ -1,18 +1,28 @@
+DEBIAN_FRONTEND=noninteractive 
+
 # Update package repository and install dependencies
 sudo apt update && \
-sudo apt install -y cryptsetup-bin libfuse2 uidmap fuse2fs fuse liblzo2-2 squashfs-tools
+sudo apt-get install -y cryptsetup-bin libfuse2 uidmap fuse2fs fuse liblzo2-2 squashfs-tools
 
 
 # Download and install go
-wget https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
+echo -n "[+] Installing GO... "
+curl -sL https://go.dev/dl/go1.23.1.linux-amd64.tar.gz -o /tmp/go1.23.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/go1.23.1.linux-amd64.tar.gz
+rm /tmp/go1.23.1.linux-amd64.tar.gz
 
 # Add go to PATH permanently
+echo -n "Adding to PATH... "
 echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
 echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
 source ~/.bashrc
+echo "Done"
+
 
 # Download singularity binary specific to ubuntu CODENAME
+echo -n "[+] Installing Singularity... "
 source /etc/os-release
-wget "https://github.com/sylabs/singularity/releases/download/v4.2.1/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb"
-sudo dpkg -i singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb
+curl -sL "https://github.com/sylabs/singularity/releases/download/v4.2.1/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb" -o /tmp/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb
+sudo dpkg -i /tmp/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb
+rm /tmp/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb
+echo "[+] Installation Complete"
