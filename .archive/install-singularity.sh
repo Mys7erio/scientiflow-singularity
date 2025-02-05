42514de4682fdb627d3dc4259f309c2b83fe2304
Shakir <shakirali.sacube@gmail.com>
2025-02-05 22:23:10 +0530

    Removed sudo from the script. The script must run as root on the commandline

--- .archive/install-singularity.sh
+++ .archive/install-singularity.sh
@@ -1,8 +1,8 @@
 DEBIAN_FRONTEND=noninteractive 
 
 # Update package repository and install dependencies
-sudo apt update && \
-sudo apt-get install -y cryptsetup-bin libfuse2 uidmap fuse2fs fuse liblzo2-2 squashfs-tools crun
+apt update && \
+apt-get install -y cryptsetup-bin libfuse2 uidmap fuse2fs fuse liblzo2-2 squashfs-tools crun
 
 
 # Download and install go
@@ -10,7 +10,7 @@ if ! go version 2>&1 > /dev/null
 then
     echo -n "[+] Go not found! Installing GO... "
     curl -sL https://go.dev/dl/go1.23.1.linux-amd64.tar.gz -o /tmp/go1.23.1.linux-amd64.tar.gz
-    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/go1.23.1.linux-amd64.tar.gz
+    rm -rf /usr/local/go && tar -C /usr/local -xzf /tmp/go1.23.1.linux-amd64.tar.gz
     rm /tmp/go1.23.1.linux-amd64.tar.gz
 
     # Add go to PATH permanently
@@ -26,6 +26,6 @@ fi
 echo -n "[+] Installing Singularity... "
 source /etc/os-release
 curl -sL "https://github.com/sylabs/singularity/releases/download/v4.2.1/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb" -o /tmp/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb
-sudo dpkg -i /tmp/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb
+dpkg -i /tmp/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb
 rm /tmp/singularity-ce_4.2.1-${UBUNTU_CODENAME}_amd64.deb
 echo "[+] Installation Complete"
