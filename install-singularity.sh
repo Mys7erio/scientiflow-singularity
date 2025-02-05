9a5515d756af1432098da826395d88055ff39111
Shakir <shakirali.sacube@gmail.com>
2025-02-05 23:22:43 +0530

    Updated singularity install script

--- install-singularity.sh
+++ install-singularity.sh
@@ -1,26 +1,25 @@
-DEBIAN_FRONTEND=noninteractive 
-
 # Update package repository and install dependencies
 apt update && \
 apt-get install -y cryptsetup-bin libfuse2 uidmap fuse2fs fuse liblzo2-2 squashfs-tools crun
 
+# Source bashrc to update PATH and check if binaries are already available
+source ~/.bashrc
 
-# Download and install go
-if ! go version 2>&1 > /dev/null
-then
-    echo -n "[+] Go not found! Installing GO... "
+# Check if Go is installed
+if ! command -v go > /dev/null 2>&1; then
+    echo -n "[+] Go not found! Installing Go... "
     curl -sL https://go.dev/dl/go1.23.1.linux-amd64.tar.gz -o /tmp/go1.23.1.linux-amd64.tar.gz
     rm -rf /usr/local/go && tar -C /usr/local -xzf /tmp/go1.23.1.linux-amd64.tar.gz
     rm /tmp/go1.23.1.linux-amd64.tar.gz
 
-    # Add go to PATH permanently
+    # Add Go to PATH permanently
     echo -n "Adding to PATH... "
     echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
     echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
-    source ~/.bashrc
     echo "Done"
+else
+    echo "[+] Go is already installed"
 fi
-
 
 # Download singularity binary specific to ubuntu CODENAME
 echo -n "[+] Installing Singularity... "
